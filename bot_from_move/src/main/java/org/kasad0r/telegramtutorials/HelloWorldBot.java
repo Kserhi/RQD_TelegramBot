c214db008376af3d898902034bd0a2631d05323f
Kserhi <movie.to.48@gmail.com>
2023-03-25 14:50:42 +0200

    Залив того бота з відео і модернізував

---
+++ bot_from_move/src/main/java/org/kasad0r/telegramtutorials/HelloWorldBot.java
@@ -0,0 +1,71 @@
+package org.kasad0r.telegramtutorials;
+
+import org.kasad0r.telegramtutorials.service.SendMessageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@Component
+public class HelloWorldBot extends TelegramLongPollingBot {
+    @Value("${telegram.bot.username}")
+    private String username;
+    @Value("${telegram.bot.token}")
+    private String token;
+
+    @Override
+    public String getBotUsername() {
+        return username;
+    }
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+    private SendMessageService sendMessageService;
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+
+            Message message = update.getMessage();
+
+            if (message.hasText()) {
+                String textFromUser =message.getText();
+                System.out.println(message.getChat().getUserName());
+                System.out.println(textFromUser);
+
+
+                switch (textFromUser){
+                    case "Створити заявку":
+                        sendMessageService.jastMesege(message,"тіпа заявка");
+                        break;
+                    case "Шось іще":
+                        sendMessageService.jastMesege(message,"ну шось там");
+                        break;
+                    default:
+                        sendMessageService.menu(message);
+                        break;
+
+                }
+
+
+
+            }
+
+
+        }
+    }
+
+
+
+
+
+
+    @Autowired
+    public void setSendMessageService(SendMessageService sendMessageService) {
+        this.sendMessageService = sendMessageService;
+    }
+}
