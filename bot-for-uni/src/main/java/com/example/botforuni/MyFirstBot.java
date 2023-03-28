9cd17011563a86335e82c62c97f8ac65a4b7ae11
Barabah26 <pavlobarabah@gmail.com>
2023-03-28 21:33:49 +0300

    Приклад з лекції

--- bot-for-uni/src/main/java/com/example/botforuni/MyFirstBot.java
+++ bot-for-uni/src/main/java/com/example/botforuni/MyFirstBot.java
@@ -1,17 +1,13 @@
 package com.example.botforuni;
 
-import com.example.botforuni.UserInfo.UserRequest;
-import com.example.botforuni.UserInfo.UserSession;
-import com.example.botforuni.service.UserSessionService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
-@Slf4j
+
 @Component
 public class MyFirstBot extends TelegramLongPollingBot {
 
@@ -26,58 +22,17 @@ public class MyFirstBot extends TelegramLongPollingBot {
         return "6139727723:AAGhYLSHJaIzSF0yDyps1b3d14PLB3oXQnI";
     }
 
-    private final Dispatcher dispatcher;
-    private final UserSessionService userSessionService;
-
-    @Autowired
-    public MyFirstBot(Dispatcher dispatcher, UserSessionService userSessionService) {
-        this.dispatcher = dispatcher;
-        this.userSessionService = userSessionService;
-    }
-
-    public MyFirstBot(DefaultBotOptions options, Dispatcher dispatcher, UserSessionService userSessionService) {
-        super(options);
-        this.dispatcher = dispatcher;
-        this.userSessionService = userSessionService;
-    }
-
     @Override
     public void onUpdateReceived(Update update) {
-        if(update.hasMessage() && update.getMessage().hasText()) {
-            String textFromUser = update.getMessage().getText();
-
-            Long userId = update.getMessage().getFrom().getId();
-            String userFirstName = update.getMessage().getFrom().getFirstName();
-
-            log.info("[{}, {}] : {}", userId, userFirstName, textFromUser);
-
-            Long chatId = update.getMessage().getChatId();
-            UserSession session = userSessionService.getSession(chatId);
-
-            UserRequest userRequest = UserRequest
-                    .builder()
-                    .update(update)
-                    .userSession(session)
-                    .chatId(chatId)
-                    .build();
-
-            boolean dispatched = dispatcher.dispatch(userRequest);
-
-            if (!dispatched) {
-                log.warn("Unexpected update from user");
-            }
-        } else {
-            log.warn("Unexpected update from user");
+        System.out.println("Повідомлення отримано: " + update.getMessage().getText());
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setText("Привіт користувач, я отримав твоє повідомлення: " + update.getMessage().getText());
+        sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
         }
-//        System.out.println("Повідомлення отримано: " + update.getMessage().getText());
-//        SendMessage sendMessage = new SendMessage();
-//        sendMessage.setText("Привіт користувач, я отримав твоє повідомлення: " + update.getMessage().getText());
-//        sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
-//        try {
-//            execute(sendMessage);
-//        } catch (TelegramApiException e) {
-//            throw new RuntimeException(e);
-//        }
 
     }
 }
--- bot-for-uni/src/main/java/com/example/botforuni/enums/ConversationState.java
+++
@@ -1,7 +0,0 @@
-package com.example.botforuni.enums;
-
-public enum ConversationState {
-        CONVERSATION_STARTED,
-        WAITING_FOR_GROUP,
-        WAITING_FOR_TEXT
-}
