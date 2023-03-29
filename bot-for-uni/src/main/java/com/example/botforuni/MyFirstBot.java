efb620965caeba465d9b7d134edcbd6aaa3008de
Barabah26 <pavlobarabah@gmail.com>
2023-03-29 21:54:33 +0300

    Відповідає на команду /start і додав кнопку

--- bot-for-uni/src/main/java/com/example/botforuni/MyFirstBot.java
+++ bot-for-uni/src/main/java/com/example/botforuni/MyFirstBot.java
@@ -4,8 +4,14 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Component
@@ -24,15 +30,37 @@ public class MyFirstBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println("Повідомлення отримано: " + update.getMessage().getText());
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setText("Привіт користувач, я отримав твоє повідомлення: " + update.getMessage().getText());
-        sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+        sendMessage.enableMarkdown(true);
+        ReplyKeyboardMarkup replyKeyboardMarkup = getKeyboard();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+
+        if (update.getMessage().getText().equals("/start")){
+            sendMessage.setText("\uD83D\uDC4BПривіт! За допомогою цього чат-бота ви зможете зробити запит до деканату!");
+            sendMessage.setChatId(String.valueOf(update.getMessage().getChatId()));
+        }
+
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
             throw new RuntimeException(e);
         }
+
+    }
+
+    private ReplyKeyboardMarkup getKeyboard(){
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow keyboardRow = new KeyboardRow();
+        keyboardRow.add("❗Потрібна послуга деканату");
+        keyboardRows.add(keyboardRow);
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+        return replyKeyboardMarkup;
 
     }
 }
