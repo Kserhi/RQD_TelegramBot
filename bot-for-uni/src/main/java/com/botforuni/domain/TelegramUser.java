3934a8d9cfb37ca3b00aad37bf3e172670d95ffc
Kserhi <movie.to.48@gmail.com>
2024-08-22 16:02:46 +0300

    add All necessary entitys for bot

---
+++ bot-for-uni/src/main/java/com/botforuni/domain/TelegramUser.java
@@ -0,0 +1,21 @@
+package com.botforuni.domain;
+
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Data
+public class TelegramUser {
+    @Id
+    private Long telegramId;
+    private String position;
+
+}
