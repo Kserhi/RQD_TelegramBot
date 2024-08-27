3f4e07623ef346591441ff34a6e1ec117d8205ab
Kserhi <movie.to.48@gmail.com>
2024-08-27 14:31:56 +0300

    refactoring

--- bot-for-uni/src/main/java/com/botforuni/TelegramBot.java
+++ bot-for-uni/src/main/java/com/botforuni/TelegramBot.java
@@ -2,6 +2,7 @@ package com.botforuni;
 
 import com.botforuni.processors.Processor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,9 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 
-@Component
 @Data
+@EqualsAndHashCode(callSuper = false) // Або callSuper = false, залежно від ваших вимог
+@Component
 public class TelegramBot extends TelegramLongPollingBot {
     @Value("${telegram.bot.username}")
     private String botUsername;
