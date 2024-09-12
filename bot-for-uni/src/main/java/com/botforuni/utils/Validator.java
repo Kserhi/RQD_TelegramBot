1a087e7eb42e2f3100c184b9735b7fb6a63ca9d6
Kserhi <movie.to.48@gmail.com>
2024-09-12 21:35:42 +0300

    add Validator

---
+++ bot-for-uni/src/main/java/com/botforuni/utils/Validator.java
@@ -0,0 +1,25 @@
+package com.botforuni.utils;
+
+public class Validator {
+
+    private static final String NAME_PATTERN = "^[a-zA-Zа-яА-ЯїЇіІєЄґҐ\\s]+$";
+    private static final String GROUP_PATTERN = "^[А-Яа-яA-Za-z]{2}\\d{2}[а-яa-z]$";
+    private static final String YEAR_PATTERN = "^\\d{4}$";
+    private static final String PHONE_PATTERN = "^\\+?\\d{10,13}$";
+
+    public static boolean validateName(String name) {
+        return name != null && name.matches(NAME_PATTERN);
+    }
+
+    public static boolean validateGroup(String group) {
+        return group != null && group.matches(GROUP_PATTERN);
+    }
+
+    public static boolean validateYear(String year) {
+        return year != null && year.matches(YEAR_PATTERN);
+    }
+
+    public static boolean validatePhoneNumber(String phoneNumber) {
+        return phoneNumber != null && phoneNumber.matches(PHONE_PATTERN);
+    }
+}
