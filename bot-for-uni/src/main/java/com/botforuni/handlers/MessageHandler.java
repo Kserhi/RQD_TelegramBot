094b83c7f12bd9ddb92dd31db087d0b62499fcaf
Kserhi <movie.to.48@gmail.com>
2024-09-13 13:46:54 +0300

    рефакторинг

--- bot-for-uni/src/main/java/com/botforuni/handlers/MessageHandler.java
+++ bot-for-uni/src/main/java/com/botforuni/handlers/MessageHandler.java
@@ -27,64 +27,98 @@ public class MessageHandler implements Handler<Message> {
 
     @Override
     public void choose(Message message) {
-        Long telegramId = message.getChatId();
-        log.info("Отримано повідомлення від користувача з ID: {}", telegramId);
+        Long chatId = message.getChatId();
+        log.info("Отримано повідомлення від користувача з ID: {}", chatId);
 
-        TelegramUserCache userCache = telegramUserService.getOrGenerate(telegramId);
-        StatementCache statementCache = userCache.getStatementCache();
+        TelegramUserCache userCache = telegramUserService.getOrGenerate(chatId);
         Position currentPosition = userCache.getPosition();
-
         log.debug("Поточна позиція користувача: {}", currentPosition);
+
+
 
         if (currentPosition != Position.NONE) {
             handleUserInputByPosition(currentPosition, message, userCache);
         } else if (message.hasText()) {
-            handleCommands(message.getText(), telegramId);
+            handleCommands(message.getText(), chatId);
         }
     }
+
+
 
     private void handleUserInputByPosition(Position position, Message message, TelegramUserCache userCache) {
         Long telegramId = userCache.getTelegramId();
         StatementCache statementCache = userCache.getStatementCache();
+        String messageText = message.getText();
+
 
         switch (position) {
-            case INPUT_USER_NAME -> handleNameInput(message.getText(), telegramId, userCache, statementCache);
-            case INPUT_USER_GROUP -> handleGroupInput(message.getText(), telegramId, userCache, statementCache);
-            case INPUT_USER_YEAR -> handleYearInput(message.getText(), telegramId, userCache, statementCache);
-            case INPUT_USER_FACULTY -> handleFacultyInput(message.getText(), telegramId, userCache, statementCache);
+            case INPUT_USER_NAME -> handleNameInput(messageText, telegramId, userCache, statementCache);
+            case INPUT_USER_GROUP -> handleGroupInput(messageText, telegramId, userCache, statementCache);
+            case INPUT_USER_YEAR -> handleYearInput(messageText, telegramId, userCache, statementCache);
+            case INPUT_USER_FACULTY -> handleFacultyInput(messageText, telegramId, userCache, statementCache);
             case INPUT_USER_PHONE -> handlePhoneInput(message, telegramId, userCache, statementCache);
             default -> log.warn("Некоректна позиція користувача: {}", position);
         }
     }
 
     private void handleNameInput(String name, Long telegramId, TelegramUserCache userCache, StatementCache statementCache) {
+        log.info("Користувач з ID: {} вводить своє ім'я: {}", telegramId, name);
         if (Validator.validateName(name)) {
             statementCache.setFullName(name);
-            updateUserPosition(userCache, statementCache, Position.INPUT_USER_GROUP, telegramId, "Введіть вашу групу (Наприклад: КН23c)⤵");
+            userCache.setPosition(Position.INPUT_USER_GROUP);
+            userCache.setStatementCache(statementCache);
+            telegramUserService.save(userCache);
+            sendMessageService.sendMessage(telegramId, "Введіть вашу групу (Наприклад: КН23c)⤵");
+            log.debug("Оновлено позицію користувача на INPUT_USER_GROUP");
+
         } else {
             sendValidationError(telegramId, "Ім'я некоректне. Будь ласка, введіть ім'я без чисел і символів.");
         }
     }
 
     private void handleGroupInput(String group, Long telegramId, TelegramUserCache userCache, StatementCache statementCache) {
+        log.info("Користувач з ID: {} вводить групу: {}", telegramId, group);
         if (Validator.validateGroup(group)) {
             statementCache.setGroupe(group);
-            updateUserPosition(userCache, statementCache, Position.INPUT_USER_YEAR, telegramId, "Введіть ваш рік набору (Наприклад: 2021)⤵");
+            userCache.setPosition(Position.INPUT_USER_YEAR);
+            userCache.setStatementCache(statementCache);
+            telegramUserService.save(userCache);
+            sendMessageService.sendMessage(telegramId, "Введіть ваш рік набору (Наприклад: 2021)⤵");
+
+            log.debug("Оновлено позицію користувача на INPUT_USER_YEAR");
         } else {
             sendValidationError(telegramId, "Група некоректна. Приклад правильного формату: КН23c.");
         }
     }
 
     private void handleYearInput(String year, Long telegramId, TelegramUserCache userCache, StatementCache statementCache) {
+        log.info("Користувач з ID: {} вводить рік набору: {}", telegramId, year);
+
         if (Validator.validateYear(year)) {
             statementCache.setYearEntry(year);
-            updateUserPosition(userCache, statementCache, Position.INPUT_USER_FACULTY, telegramId, "Виберіть ваш факультет", Keyboards.chooseFaculty());
+            userCache.setPosition(Position.INPUT_USER_FACULTY);
+            userCache.setStatementCache(statementCache);
+            telegramUserService.save(userCache);
+            sendMessageService.sendMessage(telegramId, "Виберіть ваш факультет", Keyboards.chooseFaculty());
+            log.debug("Оновлено позицію користувача на INPUT_USER_FACULTY");
+
         } else {
             sendValidationError(telegramId, "Рік некоректний. Введіть 4 цифри (Наприклад: 2021).");
         }
     }
 
     private void handleFacultyInput(String faculty, Long telegramId, TelegramUserCache userCache, StatementCache statementCache) {
+        log.info("Користувач з ID: {} вибирає факультет: {}", telegramId, faculty);
+        statementCache.setFaculty(faculty);
+        userCache.setPosition(Position.INPUT_USER_PHONE);
+        userCache.setStatementCache(statementCache);
+        telegramUserService.save(userCache);
+        sendMessageService.sendMessage(telegramId, "Введіть ваш номер телефону⤵", Keyboards.keyboardRemove());
+
+        sendMessageService.sendMessage(telegramId, "Нажміть, щоб поділитися контактом", Keyboards.phoneKeyboard());
+        log.debug("Оновлено позицію користувача на INPUT_USER_PHONE");
+
+
         statementCache.setFaculty(faculty);
         updateUserPosition(userCache, statementCache, Position.INPUT_USER_PHONE, telegramId, "Введіть ваш номер телефону⤵", Keyboards.phoneKeyboard());
     }
