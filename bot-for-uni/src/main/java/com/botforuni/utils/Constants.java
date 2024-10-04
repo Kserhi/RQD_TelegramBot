135d3a2ee0f84e8cfebcaf0f05ac5343de36114c
Kserhi <movie.to.48@gmail.com>
2024-10-04 14:52:19 +0300

    рефактиронг

--- bot-for-uni/src/main/java/com/botforuni/utils/Constants.java
+++ bot-for-uni/src/main/java/com/botforuni/utils/Constants.java
@@ -14,4 +14,11 @@ public class Constants {
     public static final String STATEMENTFORSTUDY = "Довідка з місця навчання";
 
     public static final String STATEMENTFORM9 = "Довідка (Форма 9)";
+
+    public static final String FACULTY_CIVIL_PROTECTION = "Факультет цивільного захисту";
+    public static final String FACULTY_FIRE_TECHNOLOGY_SAFETY = "Факультет пожежної та техногенної безпеки";
+    public static final String FACULTY_PSYCHOLOGY_SOCIAL_PROTECTION = "Факультет психології і соціального захисту";
+    public static final String INSTITUTE_POSTGRADUATE_EDUCATION = "Інститут післядипломної освіти";
+    public static final String ADJUNCTURE = "Ад'юктура";
+    public static final String TRAINING_METHODICAL_CENTER = "Навчально-методичний центр";
 }
